620fc3c317bd5b326f1186a9836bd5b5dbc9753d
Umberto Rosini <umbros@users.noreply.github.com>
2017-10-05 00:28:34 +0200

    identity server host configuration

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const config = {
-
-    wso2_url		: "https://identityserver:9443",
-    wso2_user		: 'admin',
-    wso2_pass 	: 'admin'
+    # wso2 config
+    wso2_url  : "https://spid-testenv-identityserver:9443",
+    wso2_user : 'admin',
+    wso2_pass : 'admin'
 
   }
 
