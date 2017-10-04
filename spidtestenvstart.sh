e12a9e76848dbea654e31a17dfbfc2c2b7b6a001
Umberto Rosini <mail@umbros.it>
2017-10-04 20:14:59 +0200

    entrypoint startup script

--- spidtestenvstart.sh
+++ spidtestenvstart.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-trap "exit" INT TERM ERR
-trap "kill 0" EXIT
-
-sh /spid-testenvironment/is/identity-server/bin/wso2server.sh start &
-sh /spid-testenvironment/is/identity-server/bin/wso2server.sh stop &
-
-wait
-
+sh /spid-testenvironment/is/identity-server/bin/wso2server.sh start
