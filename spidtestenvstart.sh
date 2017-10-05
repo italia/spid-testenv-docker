3d703e2413bc5684917f4a396af3189733cbd220
Umberto Rosini <mail@umbros.it>
2017-10-05 03:15:16 +0200

    add service start wait

--- spidtestenvstart.sh
+++ spidtestenvstart.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+# Start & Stop Identity Server to bootstrap it
 sh /spid-testenvironment/is/identity-server/bin/wso2server.sh > /dev/null &
+until $(curl --output /dev/null --silent --head --fail http://localhost:9443); do
+    printf '...'
+    sleep 3
+done
+sh /spid-testenvironment/is/identity-server/bin/wso2server.sh stop > /dev/null &
 
+# Move files
+
+
+# Run backoffice
 cd /spid-testenvironment/bo/backoffice
-npm run start-prod
+npm run start-prod &
