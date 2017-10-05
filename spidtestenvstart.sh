9aa29b5ea86e6d3be2fe3780aca64b80a34ed3d6
Umberto Rosini <mail@umbros.it>
2017-10-05 09:25:09 +0200

    is bootstrap config

--- spidtestenvstart.sh
+++ spidtestenvstart.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Start & Stop Identity Server to bootstrap it
-sh /spid-testenvironment/is/identity-server/bin/wso2server.sh > /dev/null &
+sh /spid-testenvironment/is/identity-server/bin/wso2server.sh
 until $(curl --output /dev/null --silent --head --fail http://localhost:9443); do
-    printf '...'
-    sleep 3
+    printf '.'
+    wait
 done
-sh /spid-testenvironment/is/identity-server/bin/wso2server.sh stop > /dev/null &
+sh /spid-testenvironment/is/identity-server/bin/wso2server.sh stop &
 
 # Move files
 
